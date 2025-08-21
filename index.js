a974b0147edd5f03e0c52a99eb3d9c0bdb3655ac
Serge <sannyasi326@gmail.com>
2025-08-22 02:07:37 +0300

    feat: add progress bar and volume slider

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const progress = document.querySelector('.progress');
 
 progress.addEventListener('input', function () {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value}%, #fff ${value}%, white 100%)`
+    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
 })
+
+const volumeLevel = document.querySelector('.volume-slider');
+
+volumeLevel.addEventListener('input', function () {
+    const vvalue = this.value;
+    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${vvalue}%, #c4c4c4 ${vvalue}%, #c4c4c4 100%)`
+})
+
+
+
+
+
